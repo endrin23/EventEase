85c40b03b97050007e370d2ae7e072d32ee37aaf
endrin23 <45173882+endrin23@users.noreply.github.com>
2026-03-10 23:48:23 +0100

    Add properties to EventModel class

--- EventEase/Models/EventModel.cs
+++ EventEase/Models/EventModel.cs
@@ -1,6 +1,13 @@
-﻿namespace EventEase.Models
+namespace EventEase.Models
 {
     public class EventModel
     {
+        public int Id { get; set; }
+
+        public string EventName { get; set; } = "";
+
+        public DateTime EventDate { get; set; }
+
+        public string Location { get; set; } = "";
     }
 }
